7ecb054e2f9a3bbb33ce543f0b4dc3b70c23a2dc
mbzzzzz <153828164+mbzzzzz@users.noreply.github.com>
2024-10-21 23:47:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-# Load the translation pipeline
-pipe = pipeline("text-generation", model="Alisaeed001/Llama-2-7b-chat-finetune")
-
-# Load the model and tokenizer directly
-tokenizer = AutoTokenizer.from_pretrained("Alisaeed001/Llama-2-7b-chat-finetune")
-model = AutoModelForCausalLM.from_pretrained("Alisaeed001/Llama-2-7b-chat-finetune")
-
 # Streamlit app layout
 st.title("English to Roman Urdu Chatbot")
 st.write("Enter English text below and click the button to generate a response in Roman Urdu.")
@@ -18,7 +11,8 @@ english_text = st.text_area("Input English Text")
 # Button for translation
 if st.button("Generate"):
     if english_text:
-        # Generate Roman Urdu response
+        # Load model and tokenizer only when the button is clicked
+        pipe = pipeline("text-generation", model="Alisaeed001/Llama-2-7b-chat-finetune")
         response = pipe(english_text, max_length=100, num_return_sequences=1)[0]['generated_text']
         
         # Display result
